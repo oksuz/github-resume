6d3f4297de2492c98f0c2f9b0ac3892eafffd1b9
Yunus Oksuz <yunusoksuz@gmail.com>
2020-02-12 15:18:04 +0300

    exception handler fix

--- src/Controller/ExceptionController.php
+++ src/Controller/ExceptionController.php
@@ -18,15 +18,17 @@ class ExceptionController extends AbstractController
     {
         $exception = $request->get('exception');
 
-        // ensure exception occurred while requesting to github
-        $targetHost = $exception->getRequest()->getUri()->getHost();
-        $githubApiHost = parse_url($this->getParameter('github.url'), \PHP_URL_HOST);
+        if ($exception instanceof ClientException) {
+            // ensure exception occurred while requesting to github
+            $targetHost = $exception->getRequest()->getUri()->getHost();
+            $githubApiHost = parse_url($this->getParameter('github.url'), \PHP_URL_HOST);
 
-        if ($exception instanceof ClientException && $targetHost === $githubApiHost) {
-            return $this->render('errorpage.github.html.twig', [
-                'message' => $exception->getResponse()->getBody()->__toString(),
-                'status' => $exception->getResponse()->getStatusCode()
-            ]);
+            if ($targetHost === $githubApiHost) {
+                return $this->render('errorpage.github.html.twig', [
+                    'message' => $exception->getResponse()->getBody()->__toString(),
+                    'status' => $exception->getResponse()->getStatusCode()
+                ]);
+            }
         }
 
         throw $exception;
